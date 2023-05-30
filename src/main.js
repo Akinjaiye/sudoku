50e154b9c1454624a4a467eb4f9c789e22d6eb0a
Diji Durojaiye <akindijijaiyejaiye@gmail.com>
2023-05-30 10:53:49 +0100

    add constant in main.js

--- src/main.js
+++ src/main.js
@@ -7,21 +7,21 @@ import {sudokuCheck,sudokuGen} from './js/sudoku';
 
 
 
-// const CONSTANT = {
-//     UNASSIGNED: 0,
-//     GRID_SIZE: 9,
-//     BOX_SIZE: 3,
-//     NUMBERS: [1,2,3,4,5,6,7,8,9],
-//     LEVEL_NAME: [
-//         'Easy',
-//         'Medium',
-//         'Hard',
-//         'Very hard',
-//         'Insane',
-//         'Inhuman'
-//     ],
-//     LEVEL: [29, 38, 47, 56, 65, 74]
-// }
+const CONSTANT = {
+    UNASSIGNED: 0,
+    GRID_SIZE: 9,
+    BOX_SIZE: 3,
+    NUMBERS: [1,2,3,4,5,6,7,8,9],
+    LEVEL_NAME: [
+        'Easy',
+        'Medium',
+        'Hard',
+        'Very hard',
+        'Insane',
+        'Inhuman'
+    ],
+    LEVEL: [29, 38, 47, 56, 65, 74]
+}
 
 
 // document.querySelector('#dark-mode-toggle').addEventListener('click', () => {
